61fcbf995c067e5a4a20725737d3d8a8083483b3
mabogdanets <maria.bogdanets@gmail.com>
2022-10-31 14:14:08 +0100

    fix: carousel adaptive

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,92 +1,139 @@
-import data from "./pets.json" assert { type: "json"};
+fetch("scripts/pets.json")
+  .then((res) => res.json())
+  .then((data) => {
+    const BTN_LEFT = document.querySelector("#carousel__btn_left");
+    const BTN_RIGHT = document.querySelector("#carousel__btn_right");
+    const CAROUSEL = document.querySelector("#carousel__cards");
+    const CARDS_LEFT = document.querySelector("#cards_left");
+    const CARDS_RIGHT = document.querySelector("#cards_right");
+    const CARDS_ACTIVE = document.querySelector("#cards_active");
+    
+    let numGenerate;
+    function whichNumGen(){
+      if (window.innerWidth > 1279) {
+        numGenerate = 3;      
+      } else if (window.innerWidth > 767) {
+        numGenerate = 2;
+      } else {
+        numGenerate = 1;
+      }
+      console.log("numgen in func numgen", numGenerate);
+    }
+    whichNumGen(); 
+    ///создание карточки
 
-let numGenerate = 3;
-const BTN_LEFT = document.querySelector("#carousel__btn_left");
-const BTN_RIGHT = document.querySelector("#carousel__btn_right");
-const CAROUSEL = document.querySelector("#carousel__cards");
-const CARDS_LEFT = document.querySelector("#cards_left");
-const CARDS_RIGHT = document.querySelector("#cards_right");
-const CARDS_ACTIVE = document.querySelector("#cards_active");
+    const createCard = (petObj) => {
+      const card = document.createElement("div");
+      card.classList.add("pet");
+      card.setAttribute("id", petObj.id);
 
-const createCard = (petObj) => {
-  const card = document.createElement("div");
-  card.classList.add("pet");
-  card.setAttribute("id", petObj.id);
+      const img = document.createElement("img");
+      img.classList.add("pet__img");
+      img.src = `${petObj.img}`;
+      card.append(img);
 
-  const img = document.createElement("img");
-  img.classList.add("pet__img");
-  img.src = `${petObj.img}`;
-  card.append(img);
+      const p = document.createElement("p");
+      p.classList.add("pet__name");
+      p.textContent = petObj.name;
+      card.append(p);
 
-  const p = document.createElement("p");
-  p.classList.add("pet__name");
-  p.textContent = petObj.name;
-  card.append(p);
+      const btn = document.createElement("button");
+      btn.classList.add("pet__btn");
+      btn.classList.add("button");
+      btn.classList.add("button_border");
+      btn.textContent = "Learn more";
+      card.append(btn);
 
-  const btn = document.createElement("button");
-  btn.classList.add("pet__btn");
-  btn.classList.add("button");
-  btn.classList.add("button_border");
-  btn.textContent = "Learn more";
-  card.append(btn);
+      return card;
+    }
 
-  return card;
-}
+    //// рандомный номер
 
-function getRandomNum() {
- return Math.floor(Math.random() * 8 ); 
-}
+    function getRandomNum() {
+    return Math.floor(Math.random() * 8 ); 
+    }
 
-let lastIds = "",
-    curIds = "";
-function fillCards(area){
-  for (let i = 0; i < numGenerate; i++) {
-    let num = getRandomNum();
-    while(curIds.includes(num) || lastIds.includes(num)) {
-      num = getRandomNum();
-    }  
-    curIds += num;
-    const card = createCard(data[num]);
-    area.appendChild(card);
-  }  
-  lastIds = curIds;
-  curIds = "";
-}
-fillCards(CARDS_LEFT);
-fillCards(CARDS_ACTIVE);
-fillCards(CARDS_RIGHT);
+    /// заполнить слайд карточек
 
-const moveLeft = () =>{
-  CAROUSEL.classList.add("transition-left");
-  BTN_LEFT.removeEventListener("click", moveLeft);
-  BTN_RIGHT.removeEventListener("click", moveRight);
-} 
-const moveRight = () => {
-  CAROUSEL.classList.add("transition-right");
-  BTN_LEFT.removeEventListener("click", moveLeft);
-  BTN_RIGHT.removeEventListener("click", moveRight);
-};
+    let lastIds = "",
+        curIds = "";
+    function fillCards(area){
+      console.log("numgen in func fillCards:", numGenerate);
+      for (let i = 0; i < numGenerate; i++) {
+        let num = getRandomNum();
+        while(curIds.includes(num) || lastIds.includes(num)) {
+          num = getRandomNum();
+        }  
+        curIds += num;
+        const card = createCard(data[num]);
+        area.appendChild(card);
+      }  
+      lastIds = curIds;
+      curIds = "";
+    }
 
-BTN_LEFT.addEventListener("click", moveLeft);//
-BTN_RIGHT.addEventListener("click", moveRight); //
+    function cleanAreas(){
+      CARDS_LEFT.innerHTML = "";
+      CARDS_RIGHT.innerHTML = "";
+      CARDS_ACTIVE.innerHTML = "";
+      console.log('cleanArea()');
+    }
 
-CAROUSEL.addEventListener("animationend", (animationEvent) => {
-  let changedCards;
-  if (animationEvent.animationName === "move-left") {
-    CAROUSEL.classList.remove("transition-left");
-    changedCards = CARDS_LEFT;
-    CARDS_ACTIVE.innerHTML = CARDS_LEFT.innerHTML;
-  } else {
-    CAROUSEL.classList.remove("transition-right");
-    changedCards = CARDS_RIGHT;
-    CARDS_ACTIVE.innerHTML = CARDS_RIGHT.innerHTML;
-  }
+    function fillAreas(){
+      fillCards(CARDS_LEFT);
+      fillCards(CARDS_ACTIVE);
+      fillCards(CARDS_RIGHT);
+      console.log('fillAreas()') ;
+    }
+    fillAreas();
 
-  changedCards.innerHTML = "";
-  fillCards(changedCards);
-  
-  BTN_LEFT.addEventListener("click", moveLeft);
-  BTN_RIGHT.addEventListener("click", moveRight);
-});
 
+    function changeSize() {
+      let prev = numGenerate;
+      whichNumGen();
+      console.log('не работает?', prev, numGenerate);
+
+      if (numGenerate!==prev){
+        console.log('не работает');
+        cleanAreas();
+        fillAreas(); 
+      }
+    }
+    ///нажатие кнопки, транзишион
+
+    const moveLeft = () =>{
+      CAROUSEL.classList.add("transition-left");
+      BTN_LEFT.removeEventListener("click", moveLeft);
+      BTN_RIGHT.removeEventListener("click", moveRight);
+    } 
+    const moveRight = () => {
+      CAROUSEL.classList.add("transition-right");
+      BTN_LEFT.removeEventListener("click", moveLeft);
+      BTN_RIGHT.removeEventListener("click", moveRight);
+    };
+
+    BTN_LEFT.addEventListener("click", moveLeft);//
+    BTN_RIGHT.addEventListener("click", moveRight); //
+
+    CAROUSEL.addEventListener("animationend", (animationEvent) => {
+      let changedCards;
+      if (animationEvent.animationName === "move-left") {
+        CAROUSEL.classList.remove("transition-left");
+        changedCards = CARDS_LEFT;
+        CARDS_ACTIVE.innerHTML = CARDS_LEFT.innerHTML;
+      } else {
+        CAROUSEL.classList.remove("transition-right");
+        changedCards = CARDS_RIGHT;
+        CARDS_ACTIVE.innerHTML = CARDS_RIGHT.innerHTML;
+      }
+
+      changedCards.innerHTML = "";
+      fillCards(changedCards);
+      
+      BTN_LEFT.addEventListener("click", moveLeft);
+      BTN_RIGHT.addEventListener("click", moveRight);
+    });
+
+    window.addEventListener('resize', changeSize);
+  });
 
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,44 +1,43 @@
-import data from "./pets.json" assert { type: "json"};
+fetch("scripts/pets.json")
+  .then((res) => res.json())
+  .then((data) => {
 
-const CAROUSEL = document.querySelector(".carousel__cards");
-const POPUP = document.querySelector(".wrapper-popup");
-const BODY = document.querySelector("body");
-const NAME = document.querySelector(".popup__name");
-const IMG = document.querySelector(".popup__img"); 
-const BREED = document.querySelector(".popup__breed");
-const DESCRIPT= document.querySelector(".popup__description");
-const AGE = document.querySelector(".popup__item_age");
-const INOCUL = document.querySelector(".popup__item_inoculations");
-const DISEASES= document.querySelector(".popup__item_diseases");
-const PARASITES= document.querySelector(".popup__item_parasites");
-const CAROUSEL_PETS = document.querySelector(".carousel__cards_pets");
+    const CAROUSEL = document.querySelector(".carousel__cards");
+    const POPUP = document.querySelector(".wrapper-popup");
+    const BODY = document.querySelector("body");
+    const NAME = document.querySelector(".popup__name");
+    const IMG = document.querySelector(".popup__img"); 
+    const BREED = document.querySelector(".popup__breed");
+    const DESCRIPT= document.querySelector(".popup__description");
+    const AGE = document.querySelector(".popup__item_age");
+    const INOCUL = document.querySelector(".popup__item_inoculations");
+    const DISEASES= document.querySelector(".popup__item_diseases");
+    const PARASITES= document.querySelector(".popup__item_parasites");
+    
 
+    function generatePopup(id) {
+      POPUP.classList.add("wrapper-popup_active");
+      IMG.src = `${data[id].img}`;
+      NAME.textContent = data[id].name;
+      BREED.textContent = `${data[id].type} - ${data[id].breed}`;
+      DESCRIPT.textContent = data[id].description;
+      AGE.textContent = data[id].age;
+      INOCUL.textContent = data[id].inoculations;
+      DISEASES.textContent = data[id].diseases;
+      PARASITES.textContent = data[id].parasites;
+    }
 
-function generatePopup(id) {
-  POPUP.classList.add("wrapper-popup_active");
-  IMG.src = `${data[id].img}`;
-  NAME.textContent = data[id].name;
-  BREED.textContent = `${data[id].type} - ${data[id].breed}`;
-  DESCRIPT.textContent = data[id].description;
-  AGE.textContent = data[id].age;
-  INOCUL.textContent = data[id].inoculations;
-  DISEASES.textContent = data[id].diseases;
-  PARASITES.textContent = data[id].parasites;
-}
+    CAROUSEL.addEventListener('click', (event) => {
+      if (event.target.classList.contains('pet__btn')) {
+        let id = event.target.parentElement.getAttribute("id");
+        generatePopup(id);
+        POPUP.classList.add("wrapper-popup_active");
+        BODY.classList.add("hidden-overflow-y");
+      }
+    });
 
-CAROUSEL.addEventListener('click', (event) => {
-  if (event.target.classList.contains('pet__btn')) {
-    let id = event.target.parentElement.getAttribute("id");
-    generatePopup(id);
-    POPUP.classList.add("wrapper-popup_active");
-    BODY.classList.add("hidden-overflow-y");
-  }
-});
-
-document.querySelector(".wrapper-popup").addEventListener('click', () => {
-  POPUP.classList.remove("wrapper-popup_active");
-  BODY.classList.remove("hidden-overflow-y");
-});
-
-
-
+    document.querySelector(".wrapper-popup").addEventListener('click', () => {
+      POPUP.classList.remove("wrapper-popup_active");
+      BODY.classList.remove("hidden-overflow-y");
+    });
+  });
